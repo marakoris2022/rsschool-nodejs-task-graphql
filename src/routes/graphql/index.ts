ca161374e1e156468cba8f40e7af6c4070217f09
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-04 23:16:21 +0200

    feat: added main types

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,13 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql, GraphQLEnumType } from 'graphql';
+import {
+  graphql,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './types/uuid.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -11,6 +18,56 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       BASIC: { value: 'BASIC' },
       BUSINESS: { value: 'BUSINESS' },
     },
+  });
+
+  const MemberTypeType = new GraphQLObjectType({
+    name: 'MemberType',
+    fields: () => ({
+      id: { type: MemberTypeId },
+      discount: { type: GraphQLString },
+      postsLimitPerMonth: { type: GraphQLString },
+    }),
+  });
+
+  const PostType = new GraphQLObjectType({
+    name: 'Post',
+    fields: () => ({
+      id: { type: UUIDType },
+      title: { type: GraphQLString },
+      content: { type: GraphQLString },
+      authorId: { type: new GraphQLNonNull(UUIDType) }, // Добавлено поле authorId
+    }),
+  });
+
+  const ProfileType = new GraphQLObjectType({
+    name: 'Profile',
+    fields: () => ({
+      id: { type: UUIDType },
+      isMale: { type: new GraphQLNonNull(GraphQLString) }, // Boolean
+      yearOfBirth: { type: GraphQLString },
+      userId: { type: new GraphQLNonNull(UUIDType) }, // Добавлено поле userId
+      memberType: {
+        type: MemberTypeType,
+        resolve: async (profile) => {
+          return await prisma.memberType.findUnique({
+            where: { id: profile.memberTypeId },
+          });
+        },
+      },
+    }),
+  });
+
+  const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+      id: { type: UUIDType },
+      name: { type: GraphQLString },
+      balance: { type: GraphQLString },
+      profile: { type: ProfileType },
+      posts: { type: new GraphQLList(PostType) },
+      userSubscribedTo: { type: new GraphQLList(UserType) },
+      subscribedToUser: { type: new GraphQLList(UserType) },
+    }),
   });
 
   fastify.route({
